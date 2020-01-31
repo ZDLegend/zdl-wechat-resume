d60dd4faa8d97d9dc54d7b447df3dbbd88dc9a90
张明浩 <463764941@qq.com>
2020-01-31 23:22:57 +0800

      添加基础代码（仅仅为新建工程代码）

---
+++ utils/local.js
@@ -0,0 +1,42 @@
+const LocalData = {
+  baseInfo: {
+    title: 'java开发工程师',  //职位英文名
+    name: '暗号', //姓名
+    sex: '男',
+    birthday: '1992/09/04', //生日
+    diploma: '本科', //学历
+    school: '浙江工业大学(2011-2015)',
+    workAge: '5年', //工作年限
+    location: '杭州',  //工作地点
+    wantPosition: 'java开发工程师', //期望职位
+    salary: '20K/月', //期望薪酬
+    mobilePhone: '187xxxxxxxx',//移动手机
+    email: '463764941@qq.com',//邮箱
+    github: 'ZDLegend', //github 用户名
+    githubUrl: 'https://github.com/ZDLegend', //github主页
+  },
+  workList:[
+    {
+      company:'',
+      beginTime:'',
+      endTime:'',
+      position:'',
+      worke:''
+    }
+  ],
+  projectList:[
+    {
+      name:'',
+      company:'',
+      describe:'',
+      skillTree:[
+
+      ]
+    }
+  ],
+  skillList:[
+
+  ],
+  slefDiscription:''//自我介绍
+}
+module.exports = LocalData
