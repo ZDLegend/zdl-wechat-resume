434f57a3e865f6ff9858e4c35c8f23d3070e4dfa
张明浩 <463764941@qq.com>
2020-02-12 17:27:54 +0800

    添加上划，下滑function

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,5 +15,23 @@ Page({
     this.setData({
       localDate: local
     })
+  },
+  touchStart: function (t) {
+    this.setData({
+      startY: t.changedTouches[0].clientY
+    })
+  },
+  touchEnd: function (t) {
+    let e = this, n = this.data.view;
+    this.setData({
+      endY: t.changedTouches[0].clientY
+    });
+
+    let a = t.changedTouches[0].clientY - this.data.startY;
+    if (a < -50) {
+
+    } else if (a > 50) {
+      
+    }
   }
 })
