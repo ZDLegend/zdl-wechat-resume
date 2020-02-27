c4518d1d00a5ac8e2b9c913670f9baf53a0dfb96
张明浩 <463764941@qq.com>
2020-02-27 11:03:56 +0800

    添加修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,11 @@
 //index.js
 //获取应用实例
-const local = require('../../utils/local.js')
+import LocalData from '../../utils/local.js'
+import Cain from '../../utils/Cain.js'
 
 Page({
   data: {
+    overlay: true
   },
   //事件处理函数
   bindViewTap: function() {
@@ -12,9 +14,9 @@ Page({
     })
   },
   onLoad: function () {
-    this.setData({
-      localDate: local
-    })
+    this.setData(LocalData)
+    wx.hideLoading()
+    this.setData({ 'overlay': false })
   },
   touchStart: function (t) {
     this.setData({
@@ -33,5 +35,23 @@ Page({
     } else if (a > 50) {
       
     }
+  },
+  //打开/关闭手风琴项
+  toggleAccordionItem(e) {
+    let item = this.data.workList[e.currentTarget.dataset.index];
+
+    item.isShow = !item.isShow
+
+    this.setData({
+      workList: this.data.workList
+    })
+  },
+  //设置剪切板内容
+  setClipboard(e) {
+    Cain.setClipboard(e.currentTarget.dataset.content)
+  },
+  //打电话
+  callPhone() {
+    Cain.callPhone(this.data.baseInfo.mobilePhone)
   }
 })
--- utils/local.js
+++ utils/local.js
@@ -1,11 +1,9 @@
 const LocalData = {
   baseInfo: {
     title: 'java开发工程师',  //职位英文名
-    name: '暗号', //姓名
-    sex: '男',
+    nameAndSex: '暗号 / 男', //中文名/性别
     birthday: '1992/09/04', //生日
-    diploma: '本科', //学历
-    school: '浙江工业大学(2011-2015)',
+    diplomaAndSchool: '本科 / 浙江工业大学信息工程学院(2011-2015)', //学历/学校
     workAge: '5年', //工作年限
     location: '杭州',  //工作地点
     wantPosition: 'java开发工程师', //期望职位
@@ -14,30 +12,86 @@ const LocalData = {
     email: '463764941@qq.com',//邮箱
     github: 'ZDLegend', //github 用户名
     githubUrl: 'https://github.com/ZDLegend', //github主页
-    photo:'../../image/photo.jpeg'
+    photo:'../../image/photo.jpeg',
+    wechatNo: 'zmh521125',//微信号
   },
-  workList:[
+  isShowContact: true, //是否显示联系方式
+  workList: [ //工作经验
     {
-      company:'',
-      beginTime:'',
-      endTime:'',
-      position:'',
-      worke:''
+      title: 'XXXX有限公司（2018年9月 ~ 至今）',
+      projectList: [
+        {
+          projectName: '数据平台开发',
+          duty: '代码编写',
+          info: '内部工作日志项目。',
+          skill: 'Java/Spring Boot/kafka'
+        }
+      ],
+    },
+    {
+      title: 'XXXX数字股份有限公司（2017年8月 ~ 2018年8月）',
+      projectList: [
+        {
+          projectName: '共享软件开发部',
+          duty: '共享软件管理平台',
+          info: '代码编写',
+          skill: 'Java/Spring Boot/ES'
+        },
+        {
+          projectName: '共享软件开发部',
+          duty: '共享软件中心告警系统',
+          info: '基于ebs的订单下单系统',
+          skill: 'Java/Spring Boot/Activtie'
+        }
+      ],
+    },
+    {
+      title: '浙江XX科技有限公司（2015年6月 ~ 2017月7月）',
+      projectList: [
+        {
+          projectName: '数据对接平台',
+          duty: '代码编写',
+          info: '将第三方TCP协议转为公司自有协议',
+          skill: 'Java/Spring Boot/Spring Boot Cloud/netty'
+        },
+        {
+          projectName: 'SDK-Http',
+          duty: '代码编写',
+          info: '在原有C++ SDK',
+          skill: 'Java/Spring Boot/jersey/thrift'
+        },
+        {
+          projectName: '火眼核查',
+          duty: '代码编写',
+          info: '基于ICE分布式架构',
+          skill: 'Java/ICE'
+        },
+        {
+          projectName: 'X86系列产品开发',
+          duty: '代码编写',
+          info: 'X86系列产品音频特性开发',
+          skill: 'C/C++/linux'
+        },
+      ],
     }
   ],
-  projectList:[
-    {
-      name:'',
-      company:'',
-      describe:'',
-      skillTree:[
-
-      ]
-    }
+  skillList: [  //技能清单
+    '主要计算机语言：Java',
+    '开发框架：Spring Boot',
+    '数据框架：Kafka / ES',
+    '构建工具：Maven ',
+    '数据库相关：Postgres / vatica ',
+    '版本管理：Svn / Git',
   ],
-  skillList:[
-
-  ],
-  slefDiscription:''//自我介绍
+  starList: [ //技能关键字start 1-5
+    { name: 'Java', star: 5 },
+    { name: 'Spring boot', star: 5 },
+    { name: 'Linux', star: 4 },
+    { name: 'windows', star: 4 },
+    { name: 'Git/SVN', star: 4 },
+    { name: 'javaStrap', star: 3 },
+    { name: 'C/C++', star: 3 },
+    { name: 'python', star: 2 },
+  ]
 }
 module.exports = LocalData
